24bc9c6a84519d85af5f2035d4e13792c2cdd43b
fumieval <fumiexcel@gmail.com>
2013-04-29 19:21:54 +0900

    text monad transformer

--- examples/test.hs
+++ examples/test.hs
@@ -6,7 +6,7 @@ import Control.Monad
 figureTest :: Game ()
 figureTest = do
     colored cyan $ line [V2 80 80, V2 160 160]
-    colored green $ polygon [V2 20 0, V2 100 20, V2 50 60]
+    colored green $ polygon [V2 20 0, V2 100 20, V2 90 60, V2 30 70]
     colored blue $ translate (V2 0 200) $ rotate 45 $ scale 2 $ polygonOutline [V2 20 0, V2 100 20, V2 50 60]
     colored red $ translate (V2 200 300) $ circle 40
     colored magenta $ thickness 3 $ translate (V2 100 300) $ circleOutline 50
@@ -14,8 +14,8 @@ figureTest = do
 fontTest :: Font -> Game ()
 fontTest font = do
     () <- translate (V2 100 300) $ colored black
-        $ runTextM font (BoundingBox 0 0 640 480) 17 "Hello, World"
-    return ()
+        $ runTextT Nothing font 17 "Hello, World"
+
 
 main = runGame def $ do
     font <- embedIO (loadFont "VL-PGothic-Regular.ttf")
--- free-game/Graphics/UI/FreeGame/Data/Text.hs
+++ free-game/Graphics/UI/FreeGame/Data/Text.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances, DeriveFunctor #-}
-module Graphics.UI.FreeGame.Data.Text (TextF(..), TextM, runTextM) where
+module Graphics.UI.FreeGame.Data.Text (TextF(..), TextT, runTextT) where
 
 import Data.String
 import Graphics.UI.FreeGame.Base
@@ -7,33 +7,34 @@ import Graphics.UI.FreeGame.Types
 import Graphics.UI.FreeGame.Internal.Raindrop
 import Graphics.UI.FreeGame.Data.Font
 import Graphics.UI.FreeGame.Data.Bitmap
-import Control.Monad.Free
+import Control.Monad.Trans.Free
 import Control.Monad.State
 import Linear
 
 data TextF a = TypeChar Char a deriving Functor
 
-type TextM = Free TextF
+type TextT = FreeT TextF
 
-instance IsString (TextM ()) where
+instance Monad m => IsString (TextT m ()) where
     fromString str = mapM_ (\c -> liftF (TypeChar c ())) str
 
-rewrapFree :: (Functor f, Monad m) => (f (m a) -> m a) -> Free f a -> m a
-rewrapFree _ (Pure a) = return a
-rewrapFree w (Free f) = w $ fmap (rewrapFree w) f
-
-runTextM :: (FromFinalizer m, Monad m, Picture2D m) => Maybe (BoundingBox Float) -> Font -> Float -> TextM a -> m a
-runTextM bbox font size = flip evalStateT (V2 x0 y0) . rewrapFree wr where
-    wr (TypeChar ch cont) = do
-        RenderedChar bmp (V2 x y) adv <- fromFinalizer $ charToBitmap font size ch
-        pen <- get
-        let (w,h) = bitmapSize bmp
-            offset = pen ^+^ V2 (x + fromIntegral w / 2) (y + fromIntegral h / 2)
-        translate offset $ fromBitmap bmp
-        let pen' = over _x (+adv) pen
-        put $ if cond pen'
-            then pen'
-            else V2 x0 (view _y pen + advV)
-        cont
+runTextT :: (FromFinalizer m, Monad m, Picture2D m) => Maybe (BoundingBox Float) -> Font -> Float -> TextT m a -> m a
+runTextT bbox font size = flip evalStateT (V2 x0 y0) . go where
+    go m = lift (runFreeT m) >>= \r -> case r of
+        Pure a -> return a
+        Free (TypeChar '\n' cont) -> do
+            modify $ over _x (const x0) . over _y (+advV)
+            go cont
+        Free (TypeChar ch cont) -> do
+            RenderedChar bmp (V2 x y) adv <- fromFinalizer $ charToBitmap font size ch
+            pen <- get
+            let (w,h) = bitmapSize bmp
+                offset = pen ^+^ V2 (x + fromIntegral w / 2) (y + fromIntegral h / 2)
+            translate offset $ fromBitmap bmp
+            let pen' = over _x (+adv) pen
+            put $ if cond pen'
+                then pen'
+                else V2 x0 (view _y pen + advV)
+            go cont
     advV = size * (metricsAscent font - metricsDescent font) * 1.1
     (V2 x0 y0, cond) = maybe (zero, const True) (\b -> (view _TopLeft b, flip inBoundingBox b)) bbox
